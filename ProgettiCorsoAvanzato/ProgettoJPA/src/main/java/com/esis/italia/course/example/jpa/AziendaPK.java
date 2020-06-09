f74521b853f661905035fb148e9926592315c706
mirco <mirco@tl-wr940n>
2020-06-09 10:48:59 +0200

    update aziendaPK

--- ProgettiCorsoAvanzato/ProgettoJPA/src/main/java/com/esis/italia/course/example/jpa/AziendaPK.java
+++ ProgettiCorsoAvanzato/ProgettoJPA/src/main/java/com/esis/italia/course/example/jpa/AziendaPK.java
@@ -27,6 +27,35 @@ public class AziendaPK implements Serializable{
 	public void setpIva(String pIva) {
 		this.partitaIva = pIva;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((partitaIva == null) ? 0 : partitaIva.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AziendaPK other = (AziendaPK) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (partitaIva == null) {
+			if (other.partitaIva != null)
+				return false;
+		} else if (!partitaIva.equals(other.partitaIva))
+			return false;
+		return true;
+	}
 	
 	
 }
