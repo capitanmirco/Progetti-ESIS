538692308e46b4d86b194dbfdb611197f6efd87f
mauri <mauri@DESKTOP-1FMAVBJ>
2020-06-10 12:41:02 +0200

    Mansione

---
+++ ProgettiCorsoAvanzato/ProgettoRest/src/main/java/com/esis/italia/course/example/jpa/controller/ControllerMansioneRest.java
@@ -0,0 +1,64 @@
+package com.esis.italia.course.example.jpa.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.esis.italia.course.example.jpa.entity.Impiegato;
+import com.esis.italia.course.example.jpa.entity.Mansione;
+import com.esis.italia.course.example.jpa.entity.Ruoli;
+import com.esis.italia.course.example.jpa.dao.MansioneDAO;
+import com.esis.italia.course.example.jpa.respository.MansioneRepository;
+
+@RestController
+public class ControllerMansioneRest {
+
+	@Autowired
+	private MansioneRepository repository;
+
+	private final MansioneDAO dao = new MansioneDAO();
+
+	@GetMapping("/mansione/{id}")
+	public Mansione getMansioneById(@PathVariable int id) {
+		Mansione result = null;
+
+		result = repository.findById(id);
+
+		return result;
+	}
+
+	@PostMapping("/mansione/")
+	public Mansione insertMansione(@RequestParam int idMansione, @RequestParam Impiegato impiegato, @RequestParam Ruoli ruolo) {
+
+
+		Mansione mansione = new Mansione();
+		mansione.setIdMansione(idMansione);
+		mansione.setImpiegato(impiegato);
+		mansione.setRuoli(ruolo);
+		repository.save(mansione);
+
+		return mansione;
+	}
+
+	@PutMapping("/mansione/{id}")
+	public Mansione updateMansione(@RequestParam int idMansione, @RequestParam Impiegato impiegato, @RequestParam Ruoli ruolo) {
+		Mansione mansione = null;
+
+		mansione = dao.selectByPK(Mansione.class, idMansione);
+        mansione.setImpiegato(impiegato);
+        mansione.setRuoli(ruolo);
+		repository.save(mansione);
+		return mansione;
+	}
+
+	@DeleteMapping("/mansione/{id}")
+	public boolean deleteMansione(@PathVariable int idMansione) {
+		boolean result = dao.deleteMansione(idMansione);
+		return result;
+	}
+}
